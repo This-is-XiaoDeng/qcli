327a6e5b677b85320bac27559c36a6f1b7bc5019
This-is-XiaoDeng <xiaodeng@thisisxd.tk>
2022-12-09 22:02:57 +0800

    [add]First commit

---
+++ app.py
@@ -0,0 +1,69 @@
+from textual.app import App, ComposeResult
+from textual.widgets import Header, Footer, Input, Static
+from textual.containers import Container
+
+
+class Chat(Static):
+    text = ""
+
+    def edit(self, value, a=True):
+        self.text += f"\n{value}"
+        self.update(self.text)
+        self.scroll_end()
+
+
+"""
+class Input(Static):
+    text = ""
+    def insert(self, text):
+        #if text == "backspace":
+        #    self.text = self.text[:-1]
+        #elif text == "space":
+        #    self.text += " "
+        #else:
+        self.text += text
+        self.update(self.text + "_")
+"""
+
+
+class QCli(App):
+    BINDINGS = [("d", "toggle_dark", "Toggle dark mode"),
+                ("escape", "reset_focus", "Exit Input")]
+    CSS_PATH = "vertical_layout.css"
+
+    def compose(self) -> ComposeResult:
+        """Create child widgets for the app."""
+        yield Header()
+        yield Footer()
+        self.chat = Chat()
+        yield Container(self.chat, classes="box")
+        yield Static("Seesions", classes="box")
+        # self.input = Input()
+        # yield Container(self.input, classes="box", id="two")#Static("Input", classes="box", id="two")
+        self.input = Input(classes="box", id="two")
+        yield self.input
+
+    def action_toggle_dark(self) -> None:
+        """An action to toggle dark mode."""
+        self.dark = not self.dark
+
+    async def on_input_submitted(self, event) -> None:
+        # self.chat.edit(event.value)
+        await self.send_func(event.value)
+        self.input.value = ""
+
+    def action_reset_focus(self) -> None:
+        self.input.reset_focus()
+
+    def add_message(self, text) -> None:
+        self.chat.edit(text)
+
+    def set_send_func(self, func) -> None:
+        self.send_func = func
+
+    # def on_key(self, event: events.Key):
+    #    self.input.insert(event.char)
+
+
+#app = QCli()
+# app.run()
---
+++ main.py
@@ -0,0 +1,95 @@
+from aiocqhttp import CQHttp
+import asyncio
+from aiocqhttp import Event
+import rich.console
+import app
+import rich.table
+import json
+
+
+class qCli():
+    config = json.load(open("./config.json"))
+    bot = CQHttp(
+        api_root=config["go-cqhttp"]["api_root"],
+        access_token=config["go-cqhttp"]["access_token"],
+        secret=config["go-cqhttp"]["secret"]
+    )
+    select_seesion = 701257458
+    group = True
+    ui = app.QCli()
+    console = rich.console.Console()
+    new_messages = {}
+    self_nick = None
+    self_id = None
+
+    def __init__(self):
+        pass
+
+    def start(self):
+        self.bot.on_startup(self.on_startup)
+        self.bot.on_message(self.handle_msg)
+        self.bot.run(host="127.0.0.1", port=8192)
+
+    # @bot.on_message
+    async def handle_msg(self, event: Event):
+        if self.group:
+            # console.print(event)
+            if event["message_type"] == "group":
+                # console.print(new_messages)
+                if event["group_id"] == self.select_seesion:
+                    self.ui.add_message(
+                        f'[{event["sender"]["card"] or event["sender"]["nickname"]} ({event["sender"]["user_id"]})]: {event["raw_message"]}')
+                elif event["group_id"] in self.new_messages.keys():
+                    self.new_messages[event["group_id"]] += 1
+                else:
+                    self.new_messages[event["group_id"]] = 1
+
+    async def send_message(self, message):
+        if message[0] == "/":
+            if message[:4] == "/msg":
+                if self.group:
+                    await self.bot.send_group_msg(
+                        message=message, group_id=self.select_seesion)
+            elif message[:4] == "/set":
+                if message.split(" ")[1] == "group":
+                    self.group = True
+                    self.select_seesion = int(message.split(" ")[2])
+                    self.console.rule(f"Group: {self.select_seesion}")
+            elif message[:4] == "/get":
+                if message.split(" ")[1] == "groups":
+                    table = rich.table.Table()
+                    groups = await self.bot.get_group_list()
+                    table.add_column("ID")
+                    table.add_column("Name")
+                    table.add_column("New")
+                    for g in groups:
+                        #console.print(g, new_messages)
+                        if g["group_id"] not in self.new_messages.keys():
+                            self.new_messages[g["group_id"]] = 0
+                        table.add_row(str(g["group_id"]), g["group_name"], str(
+                            self.new_messages[g["group_id"]]))
+                    self.ui.add_message(table)
+
+        elif self.group:
+            await self.bot.send_group_msg(message=message, group_id=self.select_seesion)
+            self.ui.add_message(
+                f'[{self.self_nick} ({self.self_id})]: {message}')
+
+    # @bot.on_startup
+    async def on_startup(self):
+        self.console.rule("QCLI (By XiaoDeng3386)")
+        self.self_nick = (await self.bot.get_login_info())["nickname"]
+        self.self_id = (await self.bot.get_login_info())["user_id"]
+        self.ui.set_send_func(self.send_message)
+        asyncio.create_task(self.ui.run_async())
+        #    await asyncio.sleep(5)
+        #    groups = await bot.get_group_list()
+        #    console.print(groups)
+        #    group = True
+        #    select_seesion = console.input("Group: ")
+
+
+app_class = qCli()
+app_class.start()
+# threading.Thread(target=send_message).start()
+# bot.run(host="127.0.0.1", port=8192)
